c32eb33311b118fbd232996cc084d2934cf632d6
Zepeng Li <zepengl@gmail.com>
2020-09-11 08:01:58 +0800

    add function to load e/gamma wavefrom from JUNO simulation root file.

---
+++ spectrum_classifier/load_egamma.py
@@ -0,0 +1,21 @@
+import uproot as up
+import numpy as np
+import matplotlib.pyplot as plt
+
+def LoadEGamma(rootfile, evtmax):
+    tree = up.open(rootfile)['waves']
+    wave_sum = tree.array('waves_Sum')
+    gamma_tag = tree.array('isGamma')
+    time_sequence = np.arange(len(wave_sum[0]))
+    events = []
+    for evtid in range(min(evtmax, len(wave_sum))):
+        wave_instance = wave_sum[evtid]
+        bins, edges = np.histogram(time_sequence, bins = 10,  weights = wave_instance)
+        events.append([bins, gamma_tag[evtid]])
+    return np.array(events)
+
+def test():
+    events = LoadEGamma('/hpcfs/juno/junogpu/luoxj/Data_PSD/elecsim_Electron_001_SumAllPmtWaves.root', 100)
+    print(events)
+
+#test()
