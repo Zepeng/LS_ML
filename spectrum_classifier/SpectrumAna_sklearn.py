107df27266a3a0619e53b6ad3a285fe5988e2aec
Zepeng Li <zepengl@gmail.com>
2020-09-25 10:23:19 +0800

    add test to the model training.

--- spectrum_classifier/SpectrumAna_sklearn.py
+++ spectrum_classifier/SpectrumAna_sklearn.py
@@ -48,8 +48,8 @@ class SpectrumAna():
     #load the dataset for gamma/neutron separation from calibration
     def loadcalib(self, neutronfile, gammafile):
         #load the data from root file.
-        neutron_events = load_calib.LoadCalib('/junofs/users/lirh/DYB/run67527.root', 'FastNeutron')
-        gamma_events = load_calib.LoadCalib('/junofs/users/lirh/DYB/run67522.root', 'CoTree')
+        neutron_events = load_calib.LoadCalib(neutronfile, 'FastNeutron')
+        gamma_events = load_calib.LoadCalib(gammafile, 'CoTree')
         gamma_events = gamma_events[:2*len(neutron_events)]
         #Tag the events with 0 for neutron and 1 for gamma
         neutron_tags = np.zeros(len(neutron_events))
@@ -70,7 +70,7 @@ class SpectrumAna():
                 self.target_test.append(tags[index])
 
     def loadegamma(self, rootfile):
-        events = load_egamma.LoadEGamma('/hpcfs/juno/junogpu/luoxj/Data_PSD/elecsim_SumAllPmtWaves.root')
+        events = load_egamma.LoadEGamma(rootfile)
         print(events[0])
         indices = np.arange(len(events))
         random.shuffle(indices)
@@ -128,50 +128,20 @@ class SpectrumAna():
         else:
             print('Train model %s' % modelname)
             classifier = self.models[modelname]
-            classifier.fit(input_train, target_train)
+            classifier.fit(self.input_train, self.target_train)
             pkl_filename = 'pickle_model.pkl'
             with open(pkl_filename, 'wb') as pfile:
                 pickle.dump(classifier, pfile)
+            input_test = self.input_test
+            target_test = self.target_test
+            if True:
+                predict_proba = classifier.predict_proba(input_test)
+                print(predict_proba.shape)
+                print(classifier.predict(input_test[:10]), target_test[:10])
 
-ana = SpectrumAna('FakeTest')
-ana.loadfakedata('./fake_data.npy')
-ana.AddModels()
-ana.CompareModels()
-ana.TrainModel('MLPClassifier')
-"""
-
-if True:
-    predict_proba = classifier.predict_proba(input_test)
-    print(predict_proba.shape)
-    print(classifier.predict(input_test[:10]), target_test[:10])
-    tag_0 = []
-    tag_1 = []
-    for i in range(len(predict_proba)):
-        if target_test[i] == 0:
-            tag_0.append(predict_proba[i][1])
-        else:
-            tag_1.append(predict_proba[i][1])
-    fig1, ax1 = plt.subplots()
-    n0, bins0, patches0 = ax1.hist(tag_0, bins=np.linspace(0, 1, 21), color='red', histtype='step',label='Neutron')
-    n1, bins1, patches1 = ax1.hist(tag_1, bins=np.linspace(0, 1, 21), color='blue', histtype='step', label='Co')
-    ax1.set_xlim(0,1)
-    ax1.legend()
-    ax1.set_xlabel('Prediction output')
-    fig1.savefig('predicts.png')
-
-    eff_neutron = []
-    eff_gamma = []
-    for i in range(len(n0)):
-        eff_neutron.append(np.sum(n0[i:])*1.0/np.sum(n0))
-        eff_gamma.append(np.sum(n1[i:])*1.0/np.sum(n1))
-    fig2, ax2 = plt.subplots()
-    ax2.plot(eff_neutron, eff_gamma)
-    ax2.set_xlabel('Neutron efficiency')
-    ax2.set_ylabel('Gamma efficiency')
-    ax2.set_xlim(0, 0.3)
-    ax2.set_ylim(0, 1)
-    fig2.savefig('roc.png')
-
-print("Training set score: %f" % classifier.score(input_train, target_train))
-print("Test set score: %f" % classifier.score(input_test, target_test))
-"""
+if __name__ == '__main__':
+    ana = SpectrumAna('FakeTest')
+    ana.loadfakedata('./fake_data.npy')
+    ana.AddModels()
+    ana.CompareModels()
+    ana.TrainModel('MLPClassifier')
