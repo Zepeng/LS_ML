343460b6a80d2259cf1acd723160a1cd8beaba76
Zepeng Li <zepengl@gmail.com>
2020-10-10 17:47:20 +0800

    change the data loader to use npz file instead of json.

--- cnn_vertexrecon/junodata.py
+++ cnn_vertexrecon/junodata.py
@@ -33,30 +33,19 @@ class ListDataset(data.Dataset):
         return len(self.filelist)
 
     def __getitem__(self, idx):
-        filename = self.filelist[idx]
-        pmtinfos = []
-        vertices = []
-        with open(filename, 'r') as datafile:
-            r_json = json.load(datafile)
-            for sample in r_json:
-                pmtinfos.append(np.asarray(sample['pmtinfo']))
-                vertices.append(np.asarray(sample['vertex']))
+        filename = self.filelist[int(idx/self.nevt_file)]
+        batch = np.load(filename)
+        pmtinfos = batch['pmtinfo'][idx % self.nevt_file]
+        vertices = batch['vertex'][idx % self.nevt_file]
         return torch.from_numpy(np.array(pmtinfos)).to(torch.float32),\
                 torch.from_numpy(np.array(vertices)).to(torch.float32)
 
 def test():
     list_of_datasets = []
-    filelist = glob.glob('./json_files/*.json')[:1000]
-    #for j in filelist:
-    #    if not j.endswith('.json'):
-    #        continue  # skip non-json files
-    #    list_of_datasets.append(SingleJsonDataset(json_file=j, root_dir='./', transform=None))
-    ## once all single json datasets are created you can concat them into a single one:
-    #multiple_json_dataset = data.ConcatDataset(list_of_datasets)
+    filelist = glob.glob('./out_0.npz')[:1000]
     dataset = ListDataset(filelist, 500)
-    print(dataset[0][0])
-    #for i in range(500):
-    #    print(dataset[i][0].shape)
+    for i in range(500):
+        print(dataset[i][0].shape)
 
 if __name__ == '__main__':
     test()
