4f19416536b9ab9cc1661a98e01361092c1fc2c0
Zepeng Li <zepengl@gmail.com>
2020-11-05 16:32:21 +0800

    update the script to mix signal and background.

--- cnn_classifier/DSNBDataset.py
+++ cnn_classifier/DSNBDataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 import argparse
+import ROOT
 
 class PMTIDMap():
     #The position of each pmt is stored in a root file different from the data file.
@@ -91,12 +92,72 @@ def roottonpz(mapfile, rootfile, outfile='', eventtype='sig', batchsize = 100):
         else:
             np.savez(outfile + str(batch) + 'npz', pmtinfo=np.array(pmtinfos), eventtype=np.array(types), edep=np.array(edep_batch))
 
+def chaintonpz(mapfile, sig_dir, bkg_dir, outfile='', batch_num = 100, batchsize = 500):
+    # The csv file of PMT map must have the same tag as the MC production.
+    pmtmap = PMTIDMap(mapfile)
+    pmtmap.CalcDict()
+
+    sigchain = ROOT.TChain('psdtree')
+    sigchain.Add('%s/*root' % sig_dir)
+    bkgchain = ROOT.TChain('psdtree')
+    bkgchain.Add('%s/*root' % bkg_dir)
+    #pmtids = uptree.array('PMTID')
+    #npes   = uptree.array('Charge')
+    #hittime= uptree.array('Time')
+    #edeps  = uptree.array('Edep')
+    #nbatches = int(len(pmtids)/batchsize)
+
+    pmtinfos = []
+    types = []
+    edep_batch = []
+    for batchentry in range(int(batchsize/2)):
+        #save charge and hittime to 3D array
+        i = batchsize*batch_num + batchentry
+        if i >= sigchain.GetEntries() or i >= bkgchain.GetEntries():
+            continue
+        sigchain.GetEntry(i)
+        bkgchain.GetEntry(i)
+        pmtids = sigchain.PMTID
+        npes = sigchain.Charge
+        hittime = sigchain.Time
+        edep = sigchain.Edep
+        event2dimg = np.zeros((2, 225, 126), dtype=np.float16)
+        for j in range(len(pmtids)):
+            (xbin, ybin) = pmtmap.CalcBin(pmtids[j])
+            event2dimg[0, xbin, ybin] += npes[j]
+            event2dimg[1, xbin, ybin] += hittime[j]
+        pmtinfos.append(event2dimg)
+        types.append(1)
+        edep_batch.append(edep)
+        pmtids = bkgchain.PMTID
+        npes = bkgchain.Charge
+        hittime = bkgchain.Time
+        edep = bkgchain.Edep
+        event2dimg = np.zeros((2, 225, 126), dtype=np.float16)
+        for j in range(len(pmtids)):
+            (xbin, ybin) = pmtmap.CalcBin(pmtids[j])
+            event2dimg[0, xbin, ybin] += npes[j]
+            event2dimg[1, xbin, ybin] += hittime[j]
+        pmtinfos.append(event2dimg)
+        types.append(0)
+        edep_batch.append(edep)
+
+    indices = np.arange(len(pmtinfos))
+    np.random.shuffle(indices)
+    if outfile == '':
+        np.save('data_fake.npz', pmtinfo=np.array(pmtinfos), eventtype=np.array(types))
+    else:
+        np.savez(outfile , pmtinfo=np.array(pmtinfos)[indices], eventtype=np.array(types)[indices], edep=np.array(edep_batch)[indices])
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='JUNO ML dataset builder.')
     parser.add_argument('--pmtmap', type=str, help='csc file of PMT map in JUNO.')
-    parser.add_argument('--infile', '-i', type=str, help='Input root file.')
-    parser.add_argument('--outfile', '-o', type=str, help='Output root file.')
+    parser.add_argument('--sigdir', '-s', type=str, help='Input root file.')
+    parser.add_argument('--bkgdir', '-b', type=str, help='Output root file.')
     parser.add_argument('--eventtype', '-t', type=str, help='Event type.')
+    parser.add_argument('--outfile', '-o', type=str, help='Output root file.')
+    parser.add_argument('--batch', '-n', type=int, help='Batch number.')
     args = parser.parse_args()
-    roottonpz(args.pmtmap, args.infile, args.outfile, args.eventtype)
+    #chaintonpz(args.pmtmap, args.infile, args.outfile, args.eventtype)
+    chaintonpz(args.pmtmap, args.sigdir, args.bkgdir, args.outfile, batch_num = args.batch)
     #'/cvmfs/juno.ihep.ac.cn/sl6_amd64_gcc830/Pre-Release/J19v1r1-Pre4/offline/Simulation/DetSimV2/DetSimOptions/data/PMTPos_Acrylic_with_chimney.csv', '/junofs/users/lizy/public/deeplearning/J19v1r0-Pre3/samples/train/eplus_ekin_0_10MeV/0/root_data/sample_0.root')
