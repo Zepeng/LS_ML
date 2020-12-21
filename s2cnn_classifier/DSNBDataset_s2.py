3e82fe6eb2587fca5ab65773125503add7eec0a0
Luo Xiaojie <luoxj@ihep.ac.cn>
2020-12-21 20:37:45 +0800

    fix a bug of cut

--- s2cnn_classifier/DSNBDataset_s2.py
+++ s2cnn_classifier/DSNBDataset_s2.py
@@ -189,6 +189,8 @@ def Root2npz(mapfile, sig_dir, bkg_dir, outfile='', start_entries=0):
     bkgchain.Add(bkg_dir)
 
     print("Load Raw Data Successfully!!")
+    n_sig_cut = 0
+    n_bkg_cut = 0
     pmtinfos = []
     types = []
     eqen_batch = []
@@ -210,7 +212,9 @@ def Root2npz(mapfile, sig_dir, bkg_dir, outfile='', start_entries=0):
         x = sigchain.X
         y = sigchain.Y
         z = sigchain.Z
-        if eqen <= 30 and eqen >= 11 and x ** 2 + y ** 2 + z ** 2 <= 256: #16m*16m
+        # print(f"sig   E:{eqen},R:{x ** 2 + y ** 2 + z ** 2}")
+        # if eqen <= 30 and eqen >= 11 and x ** 2 + y ** 2 + z ** 2 <= 256000000: #16m*16m
+        if eqen <= 30 and eqen >= 11 and x ** 2 + y ** 2 + z ** 2 <= 256000000:  # 16m*16m
             # print("pmtids:   ", len(pmtids)) # 24154
             # print("hittime:  ", len(hittime)) # 24154
             # print("npes:   ", len(eqen)) # 1
@@ -226,6 +230,8 @@ def Root2npz(mapfile, sig_dir, bkg_dir, outfile='', start_entries=0):
             types.append(1)
             eqen_batch.append(eqen)
             vertices.append([sigchain.X, sigchain.Y, sigchain.Z])
+        else:
+            n_sig_cut += 1;
         pmtids = bkgchain.PMTID
         npes = bkgchain.Charge
         hittime = bkgchain.Time
@@ -233,7 +239,8 @@ def Root2npz(mapfile, sig_dir, bkg_dir, outfile='', start_entries=0):
         x = bkgchain.X
         y = bkgchain.Y
         z = bkgchain.Z
-        if eqen <= 30 and eqen >= 11 and x ** 2 + y ** 2 + z ** 2 <= 256:  # 16m*16m
+        # print(f"bkg   E:{eqen},R:{x ** 2 + y ** 2 + z ** 2}")
+        if eqen <= 30 and eqen >= 11 and x ** 2 + y ** 2 + z ** 2 <= 256000000:  # 16m*16m
             event2dimg = np.zeros((2, 128, 128), dtype=np.float16)
             for j in range(len(pmtids)):
                 (xbin, ybin) = pmtmap.CalcBin(pmtids[j])
@@ -246,8 +253,10 @@ def Root2npz(mapfile, sig_dir, bkg_dir, outfile='', start_entries=0):
             types.append(0)
             vertices.append([bkgchain.X, bkgchain.Y, bkgchain.Z])
             eqen_batch.append(eqen)
+        else:
+            n_bkg_cut += 1
 
-    print(f"n_events in {outfile} : {len(pmtinfos)}")
+    print(f"n_events in {outfile} : {len(pmtinfos)}, nsig_cut: {n_sig_cut}, n_bkg_cut: {n_bkg_cut}")
     indices = np.arange(len(pmtinfos))
     np.random.shuffle(indices)
     if outfile == '':
