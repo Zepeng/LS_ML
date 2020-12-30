8031e1fce8c365b27bc811d8e9e1fcdf1417e7c8
Zepeng Li <zepengl@gmail.com>
2020-12-30 19:28:26 +0800

    script to merge hdf5 files into a single one.

--- cnn_classifier/h5merger.py
+++ cnn_classifier/h5merger.py
@@ -5,21 +5,19 @@ import h5py    # HDF5 support
 import glob
 import time
 import csv
-filelist = glob.glob('/scratchfs/exo/zepengli94/outfiles/atm_000745.h5')
+filelist = glob.glob('/scratchfs/exo/zepengli94/outfiles/*.h5')
 t1 = time.time()
 csvfile = open('dataset_info.csv', 'w')
-fieldnames = ['dsetname']
+fieldnames = ['groupname', 'dsetname']
 writer = csv.DictWriter(csvfile, fieldnames)
-with h5py.File('test.h5', 'w') as fid:
-    junodata = fid.create_group(u'juno_data')
-    for fileName in filelist:
+with h5py.File('test1.h5', 'w') as fid:
+    junodata = fid.create_group(u'juno_data' )
+    for i in range(len(filelist)):
+        fileName = filelist[i]
         print(fileName, time.time() - t1)
         f = h5py.File(fileName,  "r")
+        f.copy(f['juno_data'], junodata, name='juno_data_%d' % i)
         dset = f['juno_data']
         for item in dset.keys():
-            writer.writerow({'dsetname':item})
-            dset_copy = fid.create_dataset(item, data=dset[item])
-            dset_copy.attrs[u'tag'] = dset[item].attrs[u'tag']
-            dset_copy.attrs[u'vertex'] = dset[item].attrs[u'vertex']
-            dset_copy.attrs[u'eqen'] = dset[item].attrs[u'eqen']
+            writer.writerow({'groupname':'juno_data_%d' % i, 'dsetname':item})
         f.close()
