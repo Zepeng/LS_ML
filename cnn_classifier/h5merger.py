21d235789a51b5fdfe15dea07d35e918641a1040
Zepeng Li <zepengl@gmail.com>
2020-12-31 17:14:19 +0800

    clean up the code

--- cnn_classifier/h5merger.py
+++ cnn_classifier/h5merger.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
-'''Reads NeXus HDF5 files using h5py and prints the contents'''
-
+'''Merge hdf5 files into a single file'''
 import h5py    # HDF5 support
 import glob
 import time
 import csv
-filelist = glob.glob('/scratchfs/exo/zepengli94/outfiles/*.h5')
-t1 = time.time()
-csvfile = open('dataset_info.csv', 'w')
-fieldnames = ['groupname', 'dsetname']
-writer = csv.DictWriter(csvfile, fieldnames)
-with h5py.File('test1.h5', 'w') as fid:
-    junodata = fid.create_group(u'juno_data' )
-    for i in range(len(filelist)):
-        fileName = filelist[i]
-        print(fileName, time.time() - t1)
-        f = h5py.File(fileName,  "r")
-        f.copy(f['juno_data'], junodata, name='juno_data_%d' % i)
-        dset = f['juno_data']
-        for item in dset.keys():
-            writer.writerow({'groupname':'juno_data_%d' % i, 'dsetname':item})
-        f.close()
+
+def h5merger(filedir, csvfile, outfile):
+    filelist = glob.glob('%s/*.h5' % filedir)
+    csvfile = open(csvfile, 'w')
+    fieldnames = ['groupname', 'dsetname']
+    writer = csv.DictWriter(csvfile, fieldnames)
+    with h5py.File(outfile, 'w') as fid:
+        junodata = fid.create_group(u'juno_data' )
+        for i in range(len(filelist)):
+            fileName = filelist[i]
+            print(fileName)
+            f = h5py.File(fileName,  "r")
+            f.copy(f['juno_data'], junodata, name='juno_data_%d' % i)
+            dset = f['juno_data']
+            for item in dset.keys():
+                writer.writerow({'groupname':'juno_data_%d' % i, 'dsetname':item})
+            f.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='dataset builder.')
+    parser.add_argument('--filedir', '-f', type=str, help='directory of h5 files.')
+    parser.add_argument('--outfile', '-o', type=str, help='output h5 file.')
+    parser.add_argument('--csvfile', '-c', type=str, help='csv file of dataset info.')
+    args = parser.parse_args()
+    (args.filedir, args.csvfile, args.outfile)
