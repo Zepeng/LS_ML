fix a bug in bin index calculation.

cnn_vertexrecon/rootTodataset.py
@@ -43,10 +43,10 @@ class PMTIDMap():
     def CalcBin(self, pmtid):
         if pmtid > self.maxpmtid:
             #print('Wrong PMT ID')
-            return (0, 0)
+            return (-1, 0)
         (pmtid, x, y, z, theta, phi) = self.pmtmap[str(pmtid)]
         ybin = np.where(self.thetas == theta)[0]
-        xbin = np.where(self.thetaphi_dict[str(theta)] == phi)[0] + 113 - int(len(self.thetaphi_dict[str(theta)])/2)
+        xbin = np.where(self.thetaphi_dict[str(theta)] == phi)[0] + 112 - int(len(self.thetaphi_dict[str(theta)])/2)
         return(xbin, ybin)
 
 def uptohdf(mapfile, infile, outfile=''):
@@ -62,8 +62,8 @@ def uptohdf(mapfile, infile, outfile=''):
     edepys = uptree.array('edepY')
     edepzs = uptree.array('edepZ')
     edeps  = uptree.array('edep')
-    npes   = uptree.array('nPE')
-    hittime= uptree.array('hitTime')
+    npes   = uptree.array('npe')
+    hittime= uptree.array('hittime')
     import h5py    # HDF5 support
     import six
     import os, time
@@ -84,12 +84,14 @@ def uptohdf(mapfile, infile, outfile=''):
     junodata = f.create_group(u'juno_data')
     for entry in range(uptree.numentries):
         print(entry)
-        event2dimg = np.zeros((2, 225, 126), dtype=np.float16)
+        event2dimg = np.zeros((2, 225, len(pmtmap.thetas)), dtype=np.float16)
         for j in range(len(pmtids[entry])):
             (xbin, ybin) = pmtmap.CalcBin(pmtids[entry][j])
+            if xbin == -1:
+                continue
             event2dimg[0, xbin, ybin] += npes[entry][j]
             event2dimg[1, xbin, ybin] += hittime[entry][j]
-        dset = junodata.create_dataset(os.path.basename(outfile) + str(entry), data=event2dimg, dtype='float16')
+        dset = junodata.create_dataset(os.path.basename(outfile) + str(entry), data=event2dimg, dtype='f2')
         dset.attrs[u'vertex'] = [edepxs[entry], edepys[entry], edepzs[entry]]
         dset.attrs[u'edep'] = edeps[entry]
     f.close()