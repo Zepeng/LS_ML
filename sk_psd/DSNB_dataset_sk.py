07a18cc0cbf0603e5cd2729d44d1d615de8b9acb
Luo Xiaojie <luoxj@ihep.ac.cn>
2021-01-04 21:54:08 +0800

    change into unevent binwidth for time histgram

--- sk_psd/DSNB_dataset_sk.py
+++ sk_psd/DSNB_dataset_sk.py
@@ -17,20 +17,26 @@ def GenFilesList(dir_data:str, n_start_sig, n_start_bkg, step ):
     return (filelist_sig, filelist_bkg)
 
 def LoadData(tchain:ROOT.TChain, name_type:str, h2d:ROOT.TH2D):
-    bins_hist = range(-down_time, up_time, binwidth)
+    bins_hist = np.concatenate((np.array([-200]), np.arange(-50, 250, binwidth), np.linspace(250, 1000, 5)))
+    print(f"bins_hist: {bins_hist}")
     max_index_hist = -10
     n_beforePeak = 20
-    data_save = np.zeros((tchain.GetEntries(), 2, 420), dtype=np.float32)
+    # data_save = np.zeros((tchain.GetEntries(), 2, len(bins_hist)-1), dtype=np.float32)
+    data_save = []
     v_equen = []
     v_vertex = []
     n_tail = 400
-    for i in tqdm.tqdm(range(tchain.GetEntries())):
-    # for i in range(1,10):
+    # for i in tqdm.tqdm(range(tchain.GetEntries())):
+    for i in range(tchain.GetEntries()):
+    # for i in range(1,20):
+        if i % 200==0:
+            print(f"processing entry {i}")
         # plt.figure(name_type+str(i))
         tchain.GetEntry(i)
         pmtids = tchain.PMTID
         npes = tchain.Charge
         hittime = tchain.Time
+        # print(f"check entry {i} ,hittime: {len(hittime)}")
         eqen = tchain.Eqen
         x = tchain.X
         y = tchain.Y
@@ -39,18 +45,25 @@ def LoadData(tchain:ROOT.TChain, name_type:str, h2d:ROOT.TH2D):
         v_vertex.append([x, y, z])
         hist, bin_edges = np.histogram(hittime, bins=bins_hist )
         hist_weightE, bin_edges_weightE = np.histogram(hittime, bins=bins_hist, weights=npes)
+        # print(f"check entry {i}, hist of hittime : {hist_weightE}")
         if max_index_hist == -10:
             max_index_hist = hist.argmax()
         hist = hist/hist.max()
         hist_weightE = hist_weightE/hist_weightE.max()
-        data_save[i] = np.array([hist[max_index_hist-n_beforePeak: max_index_hist+n_tail], hist_weightE[max_index_hist-n_beforePeak: max_index_hist+n_tail]])
+        # print(f"hist_weightE:  {hist_weightE}")
+        # data_save[i] = np.array([hist, hist_weightE])
         # data_save.append(np.array([hist[max_index_hist-n_beforePeak: max_index_hist+n_tail], hist_weightE[max_index_hist-n_beforePeak: max_index_hist+n_tail]]))
+        data_save.append(np.array([hist, hist_weightE]))
+        # print(f"data_save: {np.array(data_save).shape}")
         if plot_result:
-            hist = hist_weightE[max_index_hist - n_beforePeak: max_index_hist + n_tail]
-            bin_edges = bin_edges_weightE[max_index_hist - n_beforePeak: max_index_hist + n_tail]
+            # hist = hist_weightE[max_index_hist - n_beforePeak: max_index_hist + n_tail]
+            # bin_edges = bin_edges_weightE[max_index_hist - n_beforePeak: max_index_hist + n_tail]
+            hist = hist_weightE
+            bin_edges = bin_edges_weightE
             for j_time in range(len(hist)):
                 h2d.Fill(bin_edges[j_time], hist[j_time])
         # plt.plot(hist, label=name_type+str(i))
+    # plt.legend()
     return (np.array(data_save), np.array(v_equen), np.array(v_vertex))
 def GetProfile(h2d:ROOT.TH2D):
     h_profile = h2d.ProfileX()
@@ -70,7 +83,7 @@ def PlotTimeProfile(h2d_sig:ROOT.TH2D, h2d_bkg:ROOT.TH2D):
 if __name__ == "__main__":
     up_time = 1000
     down_time = 200
-    binwidth = 2
+    binwidth = 10
     plot_result = False
     n_bins = (up_time+down_time)/binwidth
     h2d_time_sig = ROOT.TH2D("h_time_sig", "h_time_sig", int(n_bins), -down_time, up_time, int(n_bins), 0, 1.1)
@@ -104,10 +117,10 @@ if __name__ == "__main__":
     (data_save_sig, v_equen_sig, v_vertex_sig) = LoadData(sigchain, "sig", h2d_time_sig)
     (data_save_bkg, v_equen_bkg, v_vertex_bkg) = LoadData(bkgchain, "bkg", h2d_time_bkg)
     # print(f"shape:   (data_save_sig:{data_save_sig.shape}), (v_equen_sig:{v_equen_sig}), (v_vertex_sig:{v_vertex_sig})")
-    np.savez(arg.outfile, sig=data_save_sig, bkg=data_save_bkg, sig_vertex=v_vertex_sig, bkg_vertex=v_vertex_bkg, sig_equen=v_equen_sig, bkg_equen=v_equen_bkg )
+    # np.savez(arg.outfile, sig=data_save_sig, bkg=data_save_bkg, sig_vertex=v_vertex_sig, bkg_vertex=v_vertex_bkg, sig_equen=v_equen_sig, bkg_equen=v_equen_bkg )
 
     if plot_result:
-        print(f"sig_data: {data_save_sig.shape},\n bkg_data: {data_save_bkg.shape}")
+        # print(f"sig_data: {data_save_sig.shape},\n bkg_data: {data_save_bkg.shape}")
         h2d_time_bkg.SetStats(False)
         h2d_time_sig.SetStats(False)
         c_time_sig = ROOT.TCanvas("c_sig", "c_sig", 800, 600)
