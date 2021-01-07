c93e5db776722712a74d7307da36834ad86dbc0c
Luo Xiaojie <luoxj@ihep.ac.cn>
2021-01-07 16:39:44 +0800

    Add Equen cut and (R^3 & Equen) easy classify events analyze

--- sk_psd/SpectrumAna_sklearn.py
+++ sk_psd/SpectrumAna_sklearn.py
@@ -74,9 +74,9 @@ class SpectrumAna():
 
 
 
-        (data_sig_weightE, data_sig_NoweightE, data_bkg_weightE, data_bkg_NoweightE) = \
+        (data_sig_weightE, data_sig_NoweightE, data_bkg_weightE, data_bkg_NoweightE, data_sig_Equen, data_bkg_Equen, data_sig_vertex, data_bkg_vertex) = \
             self.VertexCut(data_sig_vertex, data_bkg_vertex, data_sig_weightE, data_bkg_weightE, data_sig_NoweightE,
-                           data_bkg_NoweightE, i_scheme)
+                           data_bkg_NoweightE, data_sig_equen, data_bkg_equen, i_scheme)
         # data_sig = np.concatenate((data_sig_NoweightE, data_sig_weightE, data_sig_vertex/10000, data_sig_equen.reshape(len(data_sig_equen), 1)),axis=1)
         # data_bkg = np.concatenate((data_bkg_NoweightE, data_bkg_weightE, data_bkg_vertex/10000, data_bkg_equen.reshape(len(data_bkg_equen), 1)),axis=1)
 
@@ -171,17 +171,17 @@ class SpectrumAna():
 
         models = {}
 
-        # models["LogisticRegression"]  = LogisticRegression()
-        # models["SVC"] = SVC()
-        # models["LinearSVC"] = LinearSVC()
-        # models["KNeighbors"] = KNeighborsClassifier()
-        # models["DecisionTree"] = DecisionTreeClassifier()
-        # models["RandomForest"] = RandomForestClassifier()
-        # rf2 = RandomForestClassifier(n_estimators=100, criterion='gini',
-        #                                         max_depth=10, random_state=0, max_features=None)
-        # models["RandomForest2"] = rf2
+        models["LogisticRegression"]  = LogisticRegression()
+        models["SVC"] = SVC()
+        models["LinearSVC"] = LinearSVC()
+        models["KNeighbors"] = KNeighborsClassifier()
+        models["DecisionTree"] = DecisionTreeClassifier()
+        models["RandomForest"] = RandomForestClassifier()
+        rf2 = RandomForestClassifier(n_estimators=100, criterion='gini',
+                                                max_depth=10, random_state=0, max_features=None)
+        models["RandomForest2"] = rf2
         models["MLPClassifier"] = MLPClassifier(solver='lbfgs', random_state=0)
-        # models["GaussianNB"] =  GaussianNB()
+        models["GaussianNB"] =  GaussianNB()
         self.models = models
 
     def CompareModels(self):
@@ -217,7 +217,7 @@ class SpectrumAna():
                 print(classifier.predict(input_test[:100]), target_test[:100])
 
     def VertexCut(self, data_sig_vertex, data_bkg_vertex, data_sig_weightE, data_bkg_weightE, data_sig_NoweightE,
-                  data_bkg_NoweightE, i_scheme=0):
+                  data_bkg_NoweightE, data_sig_Equen, data_bkg_Equen, i_scheme=0):
         ####make a vertex cut for dataset###############
         data_sig_R = np.sqrt(np.sum(data_sig_vertex ** 2, axis=1)) / 1000
         data_bkg_R = np.sqrt(np.sum(data_bkg_vertex ** 2, axis=1)) / 1000
@@ -242,11 +242,20 @@ class SpectrumAna():
             print("Wrong i_scheme input!!!!")
             exit(1)
 
+        if Equen_cut == True:
+            Ecut_indices_sig = ((data_sig_Equen<31) & (data_sig_Equen>10))
+            Ecut_indices_bkg = ((data_bkg_Equen<31) & (data_bkg_Equen>10))
+            cut_indices_sig = (cut_indices_sig & Ecut_indices_sig)
+            cut_indices_bkg = (cut_indices_bkg & Ecut_indices_bkg)
+
         data_sig_weightE, data_sig_NoweightE = data_sig_weightE[cut_indices_sig], data_sig_NoweightE[cut_indices_sig]
         data_bkg_weightE, data_bkg_NoweightE = data_bkg_weightE[cut_indices_bkg], data_bkg_NoweightE[cut_indices_bkg]
+        data_sig_Equen, data_bkg_Equen = data_sig_Equen[cut_indices_sig], data_bkg_Equen[cut_indices_bkg]
+        data_sig_vertex, data_bkg_vertex = data_sig_vertex[cut_indices_sig], data_bkg_vertex[cut_indices_bkg]
         print(f"check shape input : {data_bkg_weightE.shape}")
         #################################################
-        return (data_sig_weightE, data_sig_NoweightE, data_bkg_weightE, data_bkg_NoweightE)
+        return (data_sig_weightE, data_sig_NoweightE, data_bkg_weightE, data_bkg_NoweightE,
+                data_sig_Equen, data_bkg_Equen, data_sig_vertex, data_bkg_vertex)
 
     def LoadValidateData(self, datafileslist, i_scheme=0):
         # dataset = np.load(name_file, allow_pickle=True)
@@ -262,9 +271,10 @@ class SpectrumAna():
         data_sig_equen = dataset["sig_equen"]
         data_bkg_equen = dataset["bkg_equen"]
 
-        (data_sig_weightE, data_sig_NoweightE, data_bkg_weightE, data_bkg_NoweightE) = \
+        (data_sig_weightE, data_sig_NoweightE, data_bkg_weightE, data_bkg_NoweightE, data_sig_equen,
+         data_bkg_equen, data_sig_vertex, data_bkg_vertex) = \
             self.VertexCut(data_sig_vertex, data_bkg_vertex, data_sig_weightE, data_bkg_weightE, data_sig_NoweightE,
-                           data_bkg_NoweightE, i_scheme)
+                           data_bkg_NoweightE, data_sig_equen, data_bkg_equen, i_scheme)
 
         data_sig = np.concatenate((data_sig_NoweightE, data_sig_weightE), axis=1)
         data_bkg = np.concatenate((data_bkg_NoweightE, data_bkg_weightE), axis=1)
@@ -277,13 +287,18 @@ class SpectrumAna():
 
         print(f"len(data_sig):{len(data_sig)}, len(data_bkg):{len(data_bkg)}")
 
-        n_validate = len(data_bkg)
+        n_validate = np.min( [len(data_bkg), len(data_sig)] )
+        print(f"n_validate:{n_validate}")
         label_sig = np.ones(len(data_sig), dtype=np.int)
         label_bkg = np.zeros(len(data_bkg), dtype=np.int)
         self.input_validate = np.vstack((data_sig[:n_validate], data_bkg[:n_validate]))
         self.label_validate = np.concatenate((label_sig[:n_validate], label_bkg[:n_validate]))
+        self.v_vertex_validate = np.concatenate((data_sig_vertex[:n_validate], data_bkg_vertex[:n_validate]))
+        self.v_equen_validate = np.concatenate((data_sig_equen[:n_validate], data_bkg_equen[:n_validate]))
         print(f"shape of data :{self.input_validate.shape}")
-        print(f"labels : {self.label_validate}")
+        print(f" labels : {self.label_validate}")
+        print(f"shape of vertices :{self.v_vertex_validate.shape}")
+        print(f"shape of v_equqn  :{self.v_equen_validate.shape}")
         if len(self.input_validate) != len(self.label_validate):
             print("There existing a Error cause length of labels and data don't match !!!!!")
             exit(1)
@@ -378,7 +393,64 @@ class SpectrumAna():
         plt.legend()
         fig2.savefig('roc.png')
         return float(eff_sig_return)
+    def StudyEasyClassifybkg(self, name_file_model, i_scheme=0 ):
+        with open(name_file_model, 'rb') as fr:
+            classifier = pickle.load(fr)
+        predict_proba = classifier.predict_proba(self.input_validate)
+        tag_0 = []
+        tag_1 = []
+        vertex_0 = []
+        vertex_1 = []
+        v_equen_0 = []
+        v_equen_1 = []
+        for i in range(len(predict_proba)):
+            if self.label_validate[i] == 0:
+                tag_0.append(predict_proba[i][1])
+                vertex_0.append(self.v_vertex_validate[i])
+                v_equen_0.append(self.v_equen_validate[i])
+            else:
+                tag_1.append(predict_proba[i][1])
+                vertex_1.append(self.v_vertex_validate[i])
+                v_equen_1.append(self.v_equen_validate[i])
+        vertex_0, vertex_1 = np.array(vertex_0), np.array(vertex_1)
+        X_0, Y_0, Z_0 = vertex_0[:, 0], vertex_0[:, 1], vertex_0[:, 2]
+        X_1, Y_1, Z_1 = vertex_1[:, 0], vertex_1[:, 1], vertex_1[:, 2]
+        R3_0 = np.sqrt(X_0**2+Y_0**2+Z_0**2)**3/1000.**3
+        R3_1 = np.sqrt(X_1**2+Y_1**2+Z_1**2)**3/1000.**3
+        v_equen_0, v_equen_1 = np.array(v_equen_0), np.array(v_equen_1)
 
+        easy_criterion = 0.2
+        hard_criterion = 0.5
+
+        fig1 = plt.figure(f"Train scheme {i_scheme} ---R3")
+        ax1 = fig1.add_subplot(111)
+        plt.hist(R3_0[np.array(tag_0)<easy_criterion], bins=np.arange(0, 5000, 1000), histtype="step", label="$P_{signal}$"+f"<{easy_criterion}", density=True )
+        plt.hist(R3_0[np.array(tag_0)>hard_criterion], bins=np.arange(0, 5000, 1000), histtype="step", label="$P_{signal}$"+f">{hard_criterion}", density=True )
+        plt.hist(R3_0                                , bins=np.arange(0, 5000, 1000), histtype="step", label="Total",               density=True )
+        plt.title("Distribution of Background Events $R^3$ ")
+        plt.xlabel("$R^3$ [ $m^3$ ]")
+        plt.ylabel("Probability")
+        plt.legend()
+
+        fig2 = plt.figure(f"Train scheme {i_scheme} --- Equen_bkg")
+        bins_equen = np.arange(0, 70, 1)
+        plt.hist(v_equen_0[np.array(tag_0)< easy_criterion], bins=bins_equen, histtype="step", label="$P_{signal}$"+f"<{easy_criterion}", density=True)
+        plt.hist(v_equen_0[np.array(tag_0)> hard_criterion], bins=bins_equen, histtype="step", label="$P_{signal}$"+f">{hard_criterion}", density=True)
+        plt.hist(v_equen_0                                 , bins=bins_equen, histtype="step", label="Total",               density=True)
+        plt.title("Distribution of Background Events Equen ")
+        plt.xlabel("Equen [ MeV ]")
+        plt.ylabel("Probability")
+        plt.legend()
+
+        fig3 = plt.figure(f"Train scheme {i_scheme} --- Equen_sig")
+        plt.hist(v_equen_1[np.array(tag_1)< easy_criterion], bins=bins_equen, histtype="step", label="$P_{signal}$"+f"<{easy_criterion}", density=True)
+        plt.hist(v_equen_1[np.array(tag_1)> hard_criterion], bins=bins_equen, histtype="step", label="$P_{signal}$"+f">{hard_criterion}", density=True)
+        plt.hist(v_equen_1                                 , bins=bins_equen, histtype="step", label="Total",               density=True)
+        plt.title("Distribution of Signal Events Equen ")
+        plt.xlabel("Equen [ MeV ]")
+        plt.ylabel("Probability")
+        plt.legend()
+        plt.show()
 
 if __name__ == '__main__':
     import glob
@@ -386,7 +458,7 @@ if __name__ == '__main__':
     ratio_split = 0.1
     # filelist_train = filelist_total[:int((1-0.1)*len(filelist_total))]
     # filelist_validate = filelist_total[int((1-0.1)*len(filelist_total)):]
-    filelist_train = filelist_total[:30]
+    filelist_train = filelist_total[:50]
     filelist_validate = filelist_total[50:]
     print(f"filelist_train : {filelist_train}")
     print(f"filelist_validate : {filelist_validate}")
@@ -394,25 +466,28 @@ if __name__ == '__main__':
     v_eff_sig = {}
     filelist = ["test_fulltime_step10.npz", "test_fulltime_step5.npz"]
     v_eff_condition = {0:"R^3<4096", 1:"R^3<1000", 2:"1000<=R^3<2000", 3:"2000<=R^3<3000", 4:"3000<=R^3<4096"}
+    Equen_cut = True
     for i in range(5):
-    # for i in [4]:
+    # for i in [0]:
         print(f"#################processing {i} scheme##################")
         name_file_model = f"model_maxtime_{i}.pkl"
         # ana.loadDSNB("./try.npz")
-        # ana.loadDSNB(filelist_train, i_scheme=i)
-        # ana.AddModels()
-        # # # ana.CompareModels()
-        # ana.TrainModel('MLPClassifier', pkl_filename=name_file_model)
+        ana.loadDSNB(filelist_train, i_scheme=i)
+        ana.AddModels()
+        ana.CompareModels()
+        ana.TrainModel('MLPClassifier', pkl_filename=name_file_model)
 
-        ana.LoadValidateData(filelist_validate, i_scheme=i)
-        v_eff_sig[v_eff_condition[i]] = ana.LoadModelGetEfficiency(name_file_model, v_eff_condition[i])
-    print(f"Under Background eff. = 0.01, Signal eff.:{v_eff_sig}")
-    average_eff = 0
-    for key in v_eff_sig.keys():
-        if key == 'R^3<4096':
-            continue
-        average_eff += v_eff_sig[key]
-    average_eff /= (len(v_eff_sig.keys())-1)
-    print(f"Average Signal eff. : {average_eff}")
-    plt.show()
+    #     ana.LoadValidateData(filelist_validate, i_scheme=i)
+    #     # ana.StudyEasyClassifybkg(name_file_model, i_scheme=i)
+    # ######################Validate model#################################################
+    #     v_eff_sig[v_eff_condition[i]] = ana.LoadModelGetEfficiency(name_file_model, v_eff_condition[i])
+    # print(f"Under Background eff. = 0.01, Signal eff.:{v_eff_sig}")
+    # average_eff = 0
+    # for key in v_eff_sig.keys():
+    #     if key == 'R^3<4096':
+    #         continue
+    #     average_eff += v_eff_sig[key]
+    # average_eff /= (len(v_eff_sig.keys())-1)
+    # print(f"Average Signal eff. : {average_eff}")
+    # plt.show()
 
